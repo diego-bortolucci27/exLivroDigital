b972a05a359d86e66876f24ed49fdadcf5df0ae7
Diego Bortolucci <bortolucci.diego@gmail.com>
2022-02-11 15:52:14 -0300

    update na Class Pessoa

--- Pessoa.php
+++ Pessoa.php
@@ -2,7 +2,46 @@
 
     class Pessoa
     {
-        
+        private $nome;
+        private $idade;
+        private $sexo;
+
+        public function fazerAniver()
+        {
+            $this->setIdade($this->getIdade + 1);
+        }
+
+        // Metodos Especiais
+
+        public function getNome()
+        {
+            return $this->nome;
+        }
+
+        public function getIdade()
+        {
+            return $this->idade;
+        }
+
+        public function getSexo()
+        {
+            return $this->sexo;
+        }
+
+        public function setNome($n)
+        {
+            $this->nome = $n;
+        }
+
+        public function setIdade($i)
+        {
+            $this->idade = $i;
+        }
+
+        public function setSexo($s)
+        {
+            $this->sexo = $s;
+        }
     }
 
 ?>
