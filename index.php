69242c82e5389d20e4632e1c257dd66399cd701c
Diego Bortolucci <bortolucci.diego@gmail.com>
2022-02-15 11:31:32 -0300

    update index

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     $pessoa[0] = new Pessoa("Diego Bortolucci", 19, "Masculino");
     $pessoa[0]->fazerAniver();
 
-    //$pessoa2[0] = new Pessoa("Jader", 580, "Mulher");
+    $pessoa[1] = new Pessoa("Jader", 580, "Mulher");
+    $pessoa[1]->fazerAniver();
 
     echo "<h2> Livro 1 </h2>";
     $livro[0] = new Livro("Senhor dos Anéis: A Sociedade do Anel", "J.R.R Tolkien", $pessoa[0], 576);
@@ -18,10 +19,10 @@
     $livro[0]->folhear();
     $livro[0]->avancarPag();
     $livro[0]->voltarPag();
+    $livro[0]->fechar();
 
-    /*
+    
     echo "<h2> Livro 2 </h2>";
-    $livro2[0] = new Livro("O Hobbit", "J.R.R Tolkien", $pessoa2[0], 336);
-    var_dump($livro2[0]);
-    */
+    $livro[1] = new Livro("O Hobbit", "J.R.R Tolkien", $pessoa[1], 336);
+    $livro[1]->detalhes();
 ?>
